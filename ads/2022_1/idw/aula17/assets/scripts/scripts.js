f6bc69ed3aaef31b5eee957d3e565369f48b66e9
Endriw Schiavenato <endriwm.schiavenato@rede.ulbra.br>
2022-06-24 18:16:18 -0300

    24/06

--- ads/2022_1/idw/aula17/assets/scripts/scripts.js
+++ ads/2022_1/idw/aula17/assets/scripts/scripts.js
@@ -1,10 +1,9 @@
 $(document).ready(function(){
 
-
-
     function removeCss(){
         $('#menu_home'). removeClass('active');
         $('#menu_calculadora'). removeClass('active');
+        $('#menu_contadeluz'). removeClass('active');
     }
 
     $('#menu_home').click(function(){
@@ -16,7 +15,7 @@ $(document).ready(function(){
 
     $('#menu_calculadora').click(function(){
 
-        $('section').load('exercicios/calculadora.html', function(){
+        $('section').load('src/calculadora.html', function(){
             removeCss();
             $('#menu_calculadora'). addClass('active');
             $('#calcular').click(function(){
@@ -45,10 +44,31 @@ $(document).ready(function(){
                     default:
                         break;
             }
-        
             $('#result').val('' + resultado);
         
             })
         });
     })
+
+    $('#menu_contadeluz').click(function(){
+
+        $('section').load('src/contadeluz.html', function(){
+            removeCss();
+            $('#menu_contadeluz'). addClass('active');
+            $('#calcular').click(function(){
+                var quantidadeKwh = parseFloat(document.getElementById('quantidadeKwh').value);
+                var valorKwh = parseFloat(document.getElementById('valorKwh').value);
+                var valorConta = quantidadeKwh * valorKwh
+
+                if(quantidadeKwh > 100 && quantidadeKwh <=200){
+                    valorConta = valorConta * 1.25;
+                }else if(quantidadeKwh > 200){
+                    valorConta = valorConta *1.5;
+                }
+            $('#result').val('' + resultado);
+        
+            })
+        });
+    })
+
 })
