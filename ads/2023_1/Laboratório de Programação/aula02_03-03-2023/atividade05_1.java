6c8766148e97443f66260baba335932a92341519
endriwmsi <endriwm.schiavenato@rede.ulbra.br>
2023-03-13 21:40:37 -0300

    Atividades de Lab. de programação

--- ads/2023_1/Laboratório de Programação/aula02_03-03-2023/atividade05_1.java
+++ ads/2023_1/Laboratório de Programação/aula02_03-03-2023/atividade05_1.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class atividade05_1 {
+public class atividade05_1 {
+    public static void main(String[] args) {
+
+        int x = 10;
+
+        while (x <= 1000) {
+            System.out.print(x + ", ");
+            if (x % 2 == 0) {
+                x += 5;
+            } else {
+                x *= 2;
+            }
+        }
+    }
 }
--- ads/2023_1/Laboratório de Programação/aula02_03-03-2023/atividade05_2.java
+++ ads/2023_1/Laboratório de Programação/aula02_03-03-2023/atividade05_2.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class atividade05_2 {
+public class atividade05_2 {
+    public static void main(String[] args) {
+        int x = 10;
+
+        while (x <= 1000) {
+            System.out.print(x + ", ");
+            switch (x % 2) {
+                case 0:
+                    x += 5;
+                    break;
+                case 1:
+                    x *= 2;
+                    break;
+            }
+        }
+    }
 }
