59d407fbfd72a5ef711677902611ae7632a1077d
endriwmsi <endriwmsi@gmail.com>
2024-04-29 15:33:50 -0300

    First android APP

--- ads/2024_1/tecnologias-moveis/aula08_22_04_2024/app/src/main/java/com/example/cartinha/MainActivity.kt
+++ ads/2024_1/tecnologias-moveis/aula08_22_04_2024/app/src/main/java/com/example/cartinha/MainActivity.kt
@@ -4,10 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,6 +27,7 @@ class MainActivity : AppCompatActivity() {
       assunto.text.clear()
       destinatario.text.clear()
       mensagem.text.clear()
+
 
       startActivity(intent)
     }
